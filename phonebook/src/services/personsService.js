8450daaacf4fb35e18fe2ae395508bb7b0f79e48
manhhungle2906 <manhhungle2906@gmail.com>
2025-03-25 20:33:43 +0200

    fix the API baseURL

--- phonebook/src/services/personsService.js
+++ phonebook/src/services/personsService.js
@@ -1,26 +1,11 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/api/persons';
+const baseUrl = '/api/persons';
 
-// Get all persons
-const getAll = () => {
-  return axios.get(baseUrl).then(response => response.data);
-};
-
-// Add a new person
-const addPerson = (newPerson) => {
-  return axios.post(baseUrl, newPerson).then(response => response.data);
-};
-
-// Update a person's information (not used yet)
-const updatePerson = (id, updatedPerson) => {
-  return axios.put(`${baseUrl}/${id}`, updatedPerson).then(response => response.data);
-};
-
-// Delete a person
-const deletePerson = (id) => {
-  return axios.delete(`${baseUrl}/${id}`);
-};
+const getAll = () => axios.get(baseUrl).then(res => res.data);
+const addPerson = (newPerson) => axios.post(baseUrl, newPerson).then(res => res.data);
+const updatePerson = (id, updatedPerson) => axios.put(`${baseUrl}/${id}`, updatedPerson).then(res => res.data);
+const deletePerson = (id) => axios.delete(`${baseUrl}/${id}`);
 
 export default {
   getAll,
